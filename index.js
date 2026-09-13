4479eb6cceaa139f6810877704063e7deff0926e
abhi12012 <agrawalabhishek211@gmail.com>
2026-09-13 13:20:46 +0530

    Add multiply function to module

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { add, subtract } = require("./math");
+const { add, subtract, multiply } = require("./math");
 
 const name = "Abhishek";
 const age = 39;
@@ -7,3 +7,4 @@ console.log("My name is:", name);
 console.log("My age is:", age);
 console.log(add(10, 20));
 console.log(subtract(20, 5));
+console.log(multiply(10, 5));
--- math.js
+++ math.js
@@ -6,7 +6,12 @@ const subtract = (a, b) => {
   return a - b;
 };
 
+const multiply = (a, b) => {
+  return a * b;
+};
+
 module.exports = {
   add,
-  subtract
+  subtract,
+  multiply
 };
