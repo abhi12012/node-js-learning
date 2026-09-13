aee4dd71507efcb4142f9d57da0c0a6046673260
abhi12012 <agrawalabhishek211@gmail.com>
2026-09-13 13:37:05 +0530

    Learn custom user module

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const { add, subtract, multiply } = require("./math");
 
+const userName = require("./user");
+
 const name = "Abhishek";
+
 const age = 39;
 
 console.log("My name is:", name);
+console.log("User name:", userName);
+
 console.log("My age is:", age);
+
 console.log(add(10, 20));
+
 console.log(subtract(20, 5));
+
 console.log(multiply(10, 5));
---
+++ user.js
@@ -0,0 +1,3 @@
+const userName = "Abhishek";
+
+module.exports = userName;
