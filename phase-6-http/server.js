b258c6d8584456958dccc7a07d8210c35cfaf539
abhi12012 <agrawalabhishek211@gmail.com>
2026-09-18 11:12:50 +0530

    Complete Node HTTP server fundamentals

--- phase-6-http/server.js
+++ phase-6-http/server.js
@@ -2,11 +2,18 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === "/products") {
-    res.end("Products GET request");
+    const product = {
+      name: "T-Shirt",
+      price: 799,
+      category: "Clothing"
+    };
+
+
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(product));
 
   } else if (req.method === "POST" && req.url === "/products") {
     res.end("Products POST request");
-    
   } else {
     res.end("Request received");
   }
