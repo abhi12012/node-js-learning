b4ab13af636bb2f338cad41fdcaed93713bab72a
abhi12012 <agrawalabhishek211@gmail.com>
2026-09-23 10:01:54 +0530

    Add Express middleware logger

--- phase-8-express/server.js
+++ phase-8-express/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
