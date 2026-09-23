03363ffe7a3bb6ce4824950aea80478b9bd0de8b
abhi12012 <agrawalabhishek211@gmail.com>
2026-09-23 09:55:47 +0530

    Learn Express routes and JSON responses

---
+++ phase-8-express/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+
+app.get("/products", (req, res) => {
+  const category = req.query.category;
+
+  res.send(`Category is ${category}`);
+});
+
+
+app.get("/hello", (req, res) => {
+  res.send("Hello Abhishek!");
+});
+
+
+app.get("/product", (req, res) => {
+  res.json({
+    id: 1,
+    name: "Polo Shirt",
+    price: 1299,
+    category: "Clothing"
+  });
+});
+
+
+
+
+app.get("/products-json", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "Polo Shirt",
+      price: 1299
+    },
+    {
+      id: 2,
+      name: "Shoes",
+      price: 1499
+    }
+  ]);
+});
+
+
+
+
+app.get("/products/:id", (req, res) => {
+  res.send(`Product ID is ${req.params.id}`);
+});
+
+app.listen(3000, () => {
+  console.log("Express server is running on port 3000");
+});
